3cf769f5368057236baeeec1450b638ad112791b
box-archived <box.cassette@gmail.com>
2020-12-18 05:36:30 +0900

    Add utf-8 coding option

--- vlivepy/__init__.py
+++ vlivepy/__init__.py
@@ -1,2 +1,4 @@
+# -*- coding: utf-8 -*-
+
 from . import api
 from . import variables as gv
--- vlivepy/variables.py
+++ vlivepy/variables.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # VLive React App ID
 AppId = "8c6cc7b45d2568fb668be6e05b6e5a3b"
 
