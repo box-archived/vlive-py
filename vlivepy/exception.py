e4ae4ce0772099c71a4091b5f7710b4b466b3387
box-archived <box.cassette@gmail.com>
2020-12-20 13:07:33 +0900

    Add function auto_raise()
    Pass raising Exception when silent mode

--- vlivepy/exception.py
+++ vlivepy/exception.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
+# auto raise exception
+def auto_raise(exception, silent):
+    if not silent:
+        raise exception
+
+
 class APIError(Exception):
     """ Common API Error """
 
 
-class PostParseError(APIError):
-    """ Error about Post Info API """
+class APINetworkError(APIError):
+    """ Failed to load API request """
+
+
+class APIJSONParesError(APIError):
+    """ Failed to parse target """
