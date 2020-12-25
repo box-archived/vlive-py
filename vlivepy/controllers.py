359d2180ad2bbb0c9b43a37961c8070577d46dda
box-archived <box.cassette@gmail.com>
2020-12-26 08:31:17 +0900

    Refactor loadSession() to return new session

--- vlivepy/controllers.py
+++ vlivepy/controllers.py
@@ -3,29 +3,31 @@
 import pickle
 
 from .parser import sessionUserCheck
+from reqWrapper import Session
 
 
 def dumpSession(session, fp):
     r""" Dump `UserSession`
 
     :param session: valid User Session to dump
-    :type session: reqWrapper.Session
+    :type session: Session
     :param fp: file to dump
     :return: Nothing
     """
     if sessionUserCheck(session):
         pickle.dump(session.cookies, fp)
     else:
-        raise PermissionError("Can't find user data from session")
+        raise ValueError("Can't find user data from session")
 
 
-def loadSession(fp, session):
+def loadSession(fp):
     r""" load `UserSession`
 
     :param fp: file to load
-    :param session: empty Session to load User Session
-    :type session: reqWrapper.Session
-    :return: Nothing
+    :return: Loaded Session
+    :rtype: Session
     """
-
+    session = Session()
     session.cookies = pickle.load(fp)
+
+    return session
