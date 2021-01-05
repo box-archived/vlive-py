1f8bcc6c87f0999536476b26655e886c17c920ef
box-archived <box.cassette@gmail.com>
2021-01-05 11:06:59 +0900

    Add Setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='vlivepy',
+    version='0.0.1',
+    packages=['vlivepy'],
+    url='https://github.com/box-archived/vlive-py',
+    license='MIT License',
+    author='box-archived',
+    author_email='box.cassette@gmail.com',
+    description='vlivepy is reverse-engineered Python-based API of VLIVE(vlive.tv)',
+    long_description=open('README.md', encoding="utf8").read(),
+    install_requires=[
+        'requests>=2.*',
+        'reqWrapper>=0.2.*'
+    ]
+)
