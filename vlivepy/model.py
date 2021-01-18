c9e1e936c9db4b1d7a57fdb260a0a882824b4670
box-archived <box.cassette@gmail.com>
2021-01-18 22:25:17 +0900

    Refactor Upcoming's methods to resolve #1

--- vlivepy/model.py
+++ vlivepy/model.py
@@ -134,13 +134,14 @@ class Video(object):
 
 
 class Upcoming(object):
-    def __init__(self, refresh_rate=5, show_vod=True, show_upcoming=True, show_live=True):
+    def __init__(self, refresh_rate=5, show_vod=True, show_upcoming_vod=True, show_upcoming_live=True, show_live=True):
         self.refresh_rate = refresh_rate
         self.__cached_data = []
         self.__cached_time = 0
         self.show_live = show_live
         self.show_vod = show_vod
-        self.show_upcoming = show_upcoming
+        self.show_upcoming_vod = show_upcoming_vod
+        self.show_upcoming_live = show_upcoming_live
 
         # refresh data
         self.refresh(True)
@@ -148,40 +149,67 @@ class Upcoming(object):
     def refresh(self, force=False):
         distance = time() - self.__cached_time
         if distance >= self.refresh_rate or force:
-            new_data = self.load(silent=True)
+            new_data = self.load(date=None, silent=True)
             if new_data is not None:
                 self.__cached_data = new_data
                 self.__cached_time = int(time())
                 return True
         return False
 
-    def load(self, date=None, silent=False):
-        upcomings = utils.getUpcomingList(date=date, silent=silent)
-        if upcomings is not None:
-            return upcomings
+    def load(self, date, show_vod=None, show_upcoming_vod=None, show_upcoming_live=None, show_live=None, silent=False):
+        if show_live is None:
+            show_live = self.show_live
+        if show_vod is None:
+            show_vod = self.show_vod
+        if show_upcoming_vod is None:
+            show_upcoming_vod = self.show_upcoming_vod
+        if show_upcoming_live is None:
+            show_upcoming_live = self.show_upcoming_live
 
-    def upcoming(self, show_live=None, show_vod=None, show_upcoming=None):
+        upcomings = utils.getUpcomingList(date=date, silent=silent)
+
+        if upcomings is not None:
+            data_list = []
+            for item in upcomings:
+                item: UpcomingVideo
+                if item.type == "VOD" and show_vod:
+                    data_list.append(item)
+                elif item.type == "LIVE" and show_live:
+                    data_list.append(item)
+                elif item.type == "UPCOMING_VOD" and show_upcoming_vod:
+                    data_list.append(item)
+                elif item.type == "UPCOMING_LIVE" and show_upcoming_live:
+                    data_list.append(item)
+
+            return data_list
+        return None
+
+    def upcoming(self, force=False, show_vod=None, show_upcoming_vod=None, show_upcoming_live=None, show_live=None):
         r""" get upcoming list, auto refresh
 
         :return: Upcoming list
         :rtype: list[parser.upcomingVideo]
         """
-        self.refresh()
+        self.refresh(force=force)
         if show_live is None:
             show_live = self.show_live
         if show_vod is None:
             show_vod = self.show_vod
-        if show_upcoming is None:
-            show_upcoming = self.show_upcoming
+        if show_upcoming_vod is None:
+            show_upcoming_vod = self.show_upcoming_vod
+        if show_upcoming_live is None:
+            show_upcoming_live = self.show_upcoming_live
 
         data_list = []
         for item in self.__cached_data:
-            item: parser.UpcomingVideo
+            item: UpcomingVideo
             if item.type == "VOD" and show_vod:
                 data_list.append(item)
             elif item.type == "LIVE" and show_live:
                 data_list.append(item)
-            elif item.type == "UPCOMING" and show_upcoming:
+            elif item.type == "UPCOMING_VOD" and show_upcoming_vod:
+                data_list.append(item)
+            elif item.type == "UPCOMING_LIVE" and show_upcoming_live:
                 data_list.append(item)
 
         return data_list
