3d0bb5d06aad0007c76952f3c970c132455745f6
box-archived <box.cassette@gmail.com>
2021-01-18 20:27:43 +0900

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='vlivepy',
-    version='0.0.4',
+    version='0.0.5',
     packages=['vlivepy'],
     url='https://github.com/box-archived/vlive-py',
     license='MIT License',
