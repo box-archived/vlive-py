a181ad79a6cb8a0126d0125085d81e0a93304e61
box-archived <box.cassette@gmail.com>
2021-01-23 08:06:44 +0900

    Bump Version to 0.1.0

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
 setup(
     name='vlivepy',
-    version='0.0.7',
+    version='0.1.0',
     packages=['vlivepy'],
     url='https://github.com/box-archived/vlive-py',
     license='MIT License',
     author='box-archived',
     author_email='box.cassette@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description='vlivepy is reverse-engineered Python-based API of VLIVE(vlive.tv)',
     install_requires=[
         'requests>=2.*',
