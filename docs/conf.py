bb785cd1b3b68e9d5d38219ccaf121fd4c8de312
box-archived <box.cassette@gmail.com>
2021-02-06 16:27:49 +0900

    Change docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'box_archived'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'recommonmark',
+    'sphinx-rtd-theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -45,7 +46,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx-rtd-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
