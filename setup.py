497c6f3007e3ed12ac012e96a64935445a143246
box-archived <box.cassette@gmail.com>
2021-02-08 08:39:13 +0900

    Bump dev version to 0.2-dev2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name='vlivepy',
-    version='0.2-dev1',
+    version='0.2-dev2',
     packages=['vlivepy'],
     url='https://github.com/box-archived/vlive-py',
     license='MIT License',
