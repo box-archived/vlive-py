bc9235aeeac6ef156679b4a02c8d8aeb7dc2f77a
box-archived <box.cassette@gmail.com>
2021-02-16 05:58:29 +0900

    Refactor init import

--- vlivepy/__init__.py
+++ vlivepy/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from . import comment
-from . import connections
 from . import exception
 from . import post
 from . import upcoming
 from . import variables as gv
 from . import video
 from .comment import Comment
+from .connections import getUserSession, postIdToVideoSeq, postTypeDetector
 from .controllers import dumpSession, loadSession
 from .parser import UpcomingVideo
 from .post import Post
