d537d593ef04d4eea8d6553421a95749cfa0e4e9
box-archived <box.cassette@gmail.com>
2021-02-27 21:14:05 +0900

    Reformat __init__.py

--- vlivepy/__init__.py
+++ vlivepy/__init__.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from . import exception, variables, post, schedule, upcoming, video
-from .connections import getUserSession, postIdToVideoSeq, postTypeDetector
-from .controllers import dumpSession, loadSession
+from . import (
+    exception,
+    variables,
+    post,
+    schedule,
+    upcoming,
+    video,
+)
+from .connections import (
+    getUserSession,
+    postIdToVideoSeq,
+    postTypeDetector,
+)
+from .controllers import (
+    dumpSession,
+    loadSession,
+)
 from .model import (
     Comment,
     OfficialVideoPost,
     Post,
     Schedule,
     Upcoming,
-    Video,
     OfficialVideoLive,
     OfficialVideoVOD,
 )
