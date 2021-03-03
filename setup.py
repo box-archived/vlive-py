cc9c56ce919e57a2b13eaece73be81e1a972a190
box-archived <box.cassette@gmail.com>
2021-03-03 21:42:46 +0900

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name='vlivepy',
-    version='0.2.0',
+    version='0.3.0',
     packages=['vlivepy'],
     url='https://github.com/box-archived/vlive-py',
     license='MIT License',
