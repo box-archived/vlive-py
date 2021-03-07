9fc70a33b007e7f818815968550e3852101a672f
box-archived <box.cassette@gmail.com>
2021-03-07 21:52:50 +0900

    Bump RTD release version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, box-archived'
 author = 'box-archived'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.3'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
