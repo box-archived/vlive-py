2e90daa84b21e80ad04594ca693db2536bc992da
box-archived <box.cassette@gmail.com>
2021-03-10 17:29:27 +0900

    Add napoleon options

--- docs/conf.py
+++ docs/conf.py
@@ -70,3 +70,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+# napoleon
+napoleon_use_admonition_for_examples = True
