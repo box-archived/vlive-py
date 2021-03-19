7c4911a4af66e5097162c3a5cd7ecb6b85bcf261
box-archived <box.cassette@gmail.com>
2021-03-20 08:00:52 +0900

    Add docstring to channel module

--- vlivepy/channel.py
+++ vlivepy/channel.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from typing import (
+    Optional
+)
+
 from . import variables as gv
 from .exception import (
     auto_raise,
@@ -7,9 +11,25 @@ from .exception import (
 )
 from .parser import channel_info_from_channel_page
 from .router import rew_get
+from .session import UserSession
 
 
-def getChannelInfo(channel_code, session=None, silent=False):
+def getChannelInfo(
+        channel_code: str,
+        session: UserSession = None,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get detailed Channel info.
+
+    Arguments:
+        channel_code (:class:`str`, optional) : Unique id of channel to load.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
+
+    Returns:
+        :class:`dict`. Parsed channel data
+    """
+
     # Make request
     sr = rew_get(**gv.endpoint_channel_webpage(channel_code),
                  wait=0.5, session=session, status=[200])
@@ -22,7 +42,22 @@ def getChannelInfo(channel_code, session=None, silent=False):
     return None
 
 
-def getGroupedBoards(channel_code, session=None, silent=False):
+def getGroupedBoards(
+        channel_code: str,
+        session: UserSession = None,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get grouped boards info.
+
+    Arguments:
+        channel_code (:class:`str`, optional) : Unique id of channel to load boards.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
+
+    Returns:
+        :class:`dict`. Parsed json data.
+    """
+
     # Make request
     sr = rew_get(**gv.endpoint_channel_grouped_boards(channel_code),
                  wait=0.5, session=session, status=[200])
