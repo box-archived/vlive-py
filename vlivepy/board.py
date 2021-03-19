5b45763e34e11adfa134cc0abbb44beea2aaf311
box-archived <box.cassette@gmail.com>
2021-03-20 07:10:24 +0900

    Add docstring to board module

--- vlivepy/board.py
+++ vlivepy/board.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from typing import Generator, Union
+from typing import (
+    Generator,
+    Union,
+    Optional,
+)
 from . import variables as gv
 from .exception import APINetworkError, auto_raise
 from .model import OfficialVideoPost, Post
@@ -14,11 +18,11 @@ class BoardPostItem(object):
 
    Arguments:
        post_id (:class:`str`) : Unique id of post.
-       official_video (:class:`bool`) : Session for loading data with permission.
-       session (:class:`UserSession`, optional) : Session for loading data with permission.
+       official_video (:class:`bool`) : Session for loading data with permission, defaults to None.
+       session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission.
 
    Attributes:
-       session (:class:`UserSession`) : Optional. Session for loading data with permission.
+       session (:class:`vlivepy.UserSession`) : Optional. Session for loading data with permission.
    """
     __slots__ = ['__post_id', '__official_video', 'session']
 
@@ -45,35 +49,62 @@ class BoardPostItem(object):
 
     @property
     def has_official_video(self) -> bool:
+        """Boolean value for having official video
+
+        :rtype: :class:`bool`
+        """
         return self.__official_video
 
     def to_object(self) -> Union[Post, OfficialVideoPost]:
+        """Initialize matched object from post_id
+
+        Returns:
+            :class:`vlivepy.Post`, if the post is normal post.
+            :class:`vlivepy.OfficialVideoPost`, if the post is official video
+        """
         if self.__official_video:
             return OfficialVideoPost(self.post_id, session=self.session)
         else:
             return Post(self.post_id, session=self.session)
 
 
-def post_list_parser(data, session):
-    n_list = []
-    for item in data:
-        n_list.append(BoardPostItem(item['postId'],
-                                    "officialVideo" in item,
-                                    session))
+def getBoardPosts(
+        board_id: Union[str, int],
+        channel_code: str,
+        session: UserSession = None,
+        after: str = None,
+        latest: bool = False,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get board post from page
 
-    return n_list
+    Arguments:
+        board_id (:class:`str`) : Unique id of the board to load.
+        channel_code (:class:`str`) : Unique id of the channel which contains board.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        after (:class:`str`, optional) : After parameter to load another page, defaults to None.
+        latest (:class:`bool`, optional) : Load latest post first, defaults to False.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
 
-
-def getBoardPosts(board, channel_code, session=None, after=None, latest=False, silent=False):
+    Returns:
+        :class:`dict`. Parsed json data.
+    """
 
     # Make request
-    sr = rew_get(**gv.endpoint_board_posts(board, channel_code, after=after, latest=latest),
+    sr = rew_get(**gv.endpoint_board_posts(board_id, channel_code, after=after, latest=latest),
                  wait=0.5, session=session, status=[200, 403])
 
     if sr.success:
         stripped_data = response_json_stripper(sr.response.json(), silent=silent)
         if 'data' in stripped_data:
-            stripped_data['data'] = post_list_parser(stripped_data['data'], session)
+            parsed_data = []
+            for item in stripped_data['data']:
+                parsed_data.append(BoardPostItem(
+                    item['postId'],
+                    "officialVideo" in item,
+                    session
+                ))
+            stripped_data['data'] = parsed_data
         return stripped_data
     else:
         auto_raise(APINetworkError, silent)
@@ -81,15 +112,31 @@ def getBoardPosts(board, channel_code, session=None, after=None, latest=False, s
     return None
 
 
-def getBoardPostsIter(board, channel_code, session=None, latest=False) -> Generator[BoardPostItem, None, None]:
+def getBoardPostsIter(
+        board_id: Union[str, int],
+        channel_code: str,
+        session: UserSession = None,
+        latest: bool = False
+) -> Generator[BoardPostItem, None, None]:
+    """Get board post as iterable (generator).
 
-    data = getBoardPosts(board, channel_code, session=session, latest=latest)
+    Arguments:
+        board_id (:class:`str`) : Unique id of the board to load.
+        channel_code (:class:`str`) : Unique id of the channel which contains board.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        latest (:class:`str`, optional) : Load latest post first.
+
+    Yields:
+        :class:`BoardPostItem`
+    """
+
+    data = getBoardPosts(board_id, channel_code, session=session, latest=latest)
     after = next_page_checker(data)
     for item in data['data']:
         yield item
 
     while after:
-        data = getBoardPosts(board, channel_code, session=session, after=after, latest=latest)
+        data = getBoardPosts(board_id, channel_code, session=session, after=after, latest=latest)
         after = next_page_checker(data)
         for item in data['data']:
             yield item
