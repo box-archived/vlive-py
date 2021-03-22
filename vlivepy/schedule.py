48d3b29bf687ea0da937a56523dec57037e60bc5
box-archived <box.cassette@gmail.com>
2021-03-22 09:43:25 +0900

    Add docstring to schedule.py

--- vlivepy/schedule.py
+++ vlivepy/schedule.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 
+from typing import (
+    Optional
+)
+
 from . import variables as gv
 from .exception import auto_raise, APINetworkError
 from .parser import response_json_stripper
 from .router import rew_get
+from .session import UserSession
 
 
-def getScheduleData(schedule_id, session, silent=False):
-    r""" get post info
-    :param schedule_id: schedule Id from VLIVE
-    :param session: use specific session
-    :param silent: Return `None` instead of Exception
-    :return: Schedule Data
-    :rtype: dict
+def getScheduleData(
+        schedule_id: str,
+        session: UserSession,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get detailed schedule data.
+
+    Arguments:
+        schedule_id (:class:`str`) : Unique id of the schedule to load data.
+        session (:class:`vlivepy.UserSession`) : Session for loading data with permission.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
+
+    Returns:
+        :class:`dict`. Parsed json data
     """
 
     sr = rew_get(**gv.endpoint_schedule_data(schedule_id),
