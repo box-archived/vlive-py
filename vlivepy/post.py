ce858f54276a1b9915192dc789279509ccec316c
box-archived <box.cassette@gmail.com>
2021-03-22 09:38:57 +0900

    Add docstring to post.py

--- vlivepy/post.py
+++ vlivepy/post.py
@@ -1,23 +1,34 @@
 # -*- coding: utf-8 -*-
 
+from typing import (
+    Optional,
+)
+
 from . import variables as gv
 from .exception import auto_raise, APINetworkError
 from .parser import response_json_stripper
 from .router import rew_get
+from .session import UserSession
 
 
-def getFVideoInkeyData(fvideo, session=None, silent=False):
-    r""" get Inkey Data
+def getFVideoInkeyData(
+        f_video_id: str,
+        session: UserSession = None,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get InKey data of File video
 
-    :param fvideo: file video id from VLIVE (like ######)(Numbers)
-    :param session: use specific session
-    :param silent: Return `None` instead of Exception
-    :return: Inkey data
-    :rtype: dict
+    Arguments:
+        f_video_id (:class:`str`) : Unique id of the FVideo to load InKey data.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
+
+    Returns:
+        :class:`dict`. Parsed json data
     """
 
     # Make request
-    sr = rew_get(**gv.endpoint_fvideo_inkey(fvideo),
+    sr = rew_get(**gv.endpoint_fvideo_inkey(f_video_id),
                  wait=0.5, session=session, status=[200])
 
     if sr.success:
@@ -28,21 +39,31 @@ def getFVideoInkeyData(fvideo, session=None, silent=False):
     return None
 
 
-def getFVideoPlayInfo(videoSeqId, videoVodId, session=None, silent=False):
-    r""" Get fvideo VOD Data
+def getFVideoPlayInfo(
+        f_video_id: str,
+        f_vod_id: str,
+        session: UserSession = None,
+        silent: bool = False
+) -> Optional[dict]:
+    """Get InKey data of File video
 
-    :param videoSeqId: videoId from attachment-id (like #-########)
-    :param videoVodId: videoId from attachment/uploadInfo (like #-########)
-    :param session: use specific session
-    :param silent: Return `None` instead of Exception
-    :return:
+    Arguments:
+        f_video_id (:class:`str`) : Unique id of the video-attachment to load data.
+        f_vod_id (:class:`str`) : Unique id of the video-vod to load data.
+        session (:class:`vlivepy.UserSession`, optional) : Session for loading data with permission, defaults to None.
+        silent (:class:`bool`, optional) : Return None instead of raising exception, defaults to False.
+
+    Returns:
+        :class:`dict`. Parsed json data
     """
 
-    inkey = getFVideoInkeyData(fvideo=videoSeqId, session=session)
-    sr = rew_get(**gv.endpoint_vod_play_info(videoVodId, inkey),
+    inkey = getFVideoInkeyData(f_video_id=f_video_id, session=session)
+    sr = rew_get(**gv.endpoint_vod_play_info(f_vod_id, inkey),
                  session=session, wait=0.3, status=[200, 403])
 
     if sr.success:
         return response_json_stripper(sr.response.json(), silent=silent)
     else:
         auto_raise(APINetworkError, silent=silent)
+
+    return None
